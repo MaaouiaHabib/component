58fe92a0da1ba14bb8bdbc6de01c93837eaca2cc
habib maaouia <maaouia.habib@gmail.com>
2021-08-11 14:20:40 +0200

    testing

---
+++ compo/src/COMPONENT/PROFILE/Adress.js
@@ -0,0 +1,9 @@
+import React from "react";
+const App = () => {
+ return (
+   <>
+     <h2>My Adress</h2>
+   </>
+ );
+};
+export default App;
---
+++ compo/src/COMPONENT/PROFILE/FullName.js
@@ -0,0 +1,9 @@
+import React from "react";
+const App = () => {
+ return (
+   <>
+     <h2>Habib Maaouia</h2>
+   </>
+ );
+};
+export default App;
